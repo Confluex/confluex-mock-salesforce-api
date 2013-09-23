82ab5f9e29d26e08c20d0100c68dab5e522aa128
Ryan Hoegg <ryan.hoegg@gmail.com>
2013-09-23 12:06:40 -0500

    Capture requests to SForce API for later assertions

---
+++ src/main/groovy/com/confluex/test/salesforce/sforce/SforceRequest.groovy
@@ -0,0 +1,11 @@
+package com.confluex.test.salesforce.sforce
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryan
+ * Date: 9/23/13
+ * Time: 11:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class SforceRequest {
+}
---
+++ src/main/groovy/com/confluex/test/salesforce/sforce/SforceUpdateRequest.groovy
@@ -0,0 +1,11 @@
+package com.confluex.test.salesforce.sforce
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryan
+ * Date: 9/23/13
+ * Time: 11:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class SforceUpdateRequest {
+}
